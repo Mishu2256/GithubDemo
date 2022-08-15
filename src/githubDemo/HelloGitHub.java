729e2e18ee477c33c30512e0d80515ea763c9c46
mishu2256 <mdkhalequzzman@mds-MacBook-Pro.local>
2022-08-15 17:27:07 -0400

    hw

--- src/githubDemo/HelloGitHub.java
+++ src/githubDemo/HelloGitHub.java
@@ -2,7 +2,7 @@ package githubDemo;
 
 public class HelloGitHub {
 	public static void main(String[] args) {
-		System.out.println("I know how to create a separate remote repository in Github and can push my project GithubDemo, without help of anyone");
+		System.out.println("I Know How To create a separate remote repository in Github and can push my project GithubDemo, without help of anyone");
 	}
 
 }
